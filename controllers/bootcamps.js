aadae1fc3ff10793629537331e40a326bdc6e033
ringkurahman <mrringkurahman@gmail.com>
2020-12-18 23:32:41 +0400

    Fetch all bootcamps and single bootcamps by id

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,14 +6,37 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' })
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+
+        res.status(200).json({
+            success: true,
+            data: bootcamps
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
 
 // @desc        Get single bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
-exports.getBootcamp = (req, res, next) => {
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+
+        if (!bootcamp) {
+            return res.status(404).json({ success: false })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
     res.status(200).json({ success: true, msg: `Get single bootcamp ${req.params.id}` })
 }
 
