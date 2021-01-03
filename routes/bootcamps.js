7075f624ffc41969d9ee2265683394f0bdb33784
ringkurahman <mrringkurahman@gmail.com>
2021-01-03 21:03:46 +0400

    Added middleware for pagination and limit, select sort

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,6 +8,10 @@ const {
     getBootcampsInRadius,
     bootcampPhotoUpload
 } = require('../controllers/bootcamps')
+const Bootcamp = require('../models/Bootcamp')
+const advancedResults = require('../middleware/advancedResults')
+
+
 
 // Include other resource routes
 const courseRouter = require('./courses')
@@ -18,8 +22,11 @@ const router = express.Router()
 router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
+
 router.route('/:id/photo').put(bootcampPhotoUpload)
-router.route('/').get(getBootcamps).post(createBootcamp)
+
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp)
+
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
 module.exports = router
