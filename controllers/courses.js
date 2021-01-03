34bfe745cf90162e4e3523a469869a9c4f89abfe
ringkurahman <mrringkurahman@gmail.com>
2021-01-03 13:04:00 +0400

    Update and delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -53,10 +53,10 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 exports.addCourse = asyncHandler(async (req, res, next) => {
     req.body.bootcamp = req.params.bootcampId
 
-    const bootcamp = await Bootcamp.findById(req.body.bootcamp)
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId)
 
     if (!bootcamp) {
-        return next(new ErrorResponse(`Bootcamp not found with id of ${req.body.bootcamp}`, 404))
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.bootcampId}`, 404))
     }
 
     const course = await Course.create(req.body)
@@ -64,6 +64,47 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     res.status(200).json({
         success: true,
         data: course
+    })
+})
+
+// @desc        Update course
+// @route       PUT /api/v1/courses/:id
+// @access      Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+
+    let course = await Course.findById(req.params.id)
+
+    if (!course) {
+        return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 404))
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+        
+    res.status(200).json({
+        success: true,
+        data: course
+    })
+})
+
+// @desc        Delete course
+// @route       Delete /api/v1/courses/:id
+// @access      Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+
+    const course = await Course.findById(req.params.id)
+
+    if (!course) {
+        return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 404))
+    }
+
+    await course.remove()
+        
+    res.status(200).json({
+        success: true,
+        data: {}
     })
 })
 
