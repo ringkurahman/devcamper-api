ddb68c2694cb32cacd814ce73a72ad71a77a6be2
ringkurahman <mrringkurahman@gmail.com>
2021-01-04 18:55:40 +0400

    Added login method

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const User = require('../models/User')
 
 
 // @desc        Register user
-// @route       GET /api/v1/auth/register
+// @route       POST /api/v1/auth/register
 // @access      Public
 exports.register = asyncHandler(async (req, res, next) => {
     
@@ -18,6 +18,39 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     })
+
+    // Create Token with getSignedJwtToken method from user model
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({ success: true, token })
+})
+
+
+// @desc        Login user
+// @route       POST /api/v1/auth/login
+// @access      Public
+exports.login = asyncHandler(async (req, res, next) => {
+    
+    const { email, password } = req.body
+
+    // validate email and password
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400))
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401))
+    }
+
+    // Check if password matches with matchPassword method from user model
+    const isMatch = await user.matchPassword(password)
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401))
+    }
 
     // Create Token with getSignedJwtToken method from user model
     const token = user.getSignedJwtToken()
