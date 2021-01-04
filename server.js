83a6826dce50b4cbd2952e8826f43b1a4971b29b
ringkurahman <mrringkurahman@gmail.com>
2021-01-04 16:56:55 +0400

    Added user register functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
+const auth = require('./routes/auth')
 const dotenv = require('dotenv')
 dotenv.config({ path: './config/config.env' })
 const morgan = require('morgan')
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Mount router
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
 
 // Error Handler
 app.use(errorHandler)
