8116c238989c60a0fe81a416e5e1fe3ef82b2f3d
ringkurahman <mrringkurahman@gmail.com>
2021-01-05 20:27:15 +0400

    Created update user details and password functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth')
+const { register, login, getMe, updateDetails, updatePassword, forgotPassword, resetPassword } = require('../controllers/auth')
 
 const router = express.Router()
 
@@ -8,8 +8,11 @@ const { protect } = require('../middleware/auth')
 router.post('/register', register)
 router.post('/login', login)
 router.get('/me', protect, getMe)
+router.put('/updatedetails', protect, updateDetails)
+router.put('/updatepassword', protect, updatePassword)
 router.post('/forgotpassword', forgotPassword)
 router.put('/resetpassword/:resettoken', resetPassword)
+
 
 
 module.exports = router
